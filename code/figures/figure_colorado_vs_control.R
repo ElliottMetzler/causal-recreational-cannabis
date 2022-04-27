219a6e78b87ee5a55599031089476b0a77258238
pmassey191 <pmassey@utexas.edu>
2022-04-26 23:23:31 -0500

    Update figure_colorado_vs_control.R

--- code/figures/figure_colorado_vs_control.R
+++ code/figures/figure_colorado_vs_control.R
@@ -1,4 +1,4 @@
-df <- df <- read_csv(here("data", "clean", "clean.csv"),
+df <- df <- read_csv(here("data", "clean", "clean_2000.csv"),
                      show_col_types = F) %>% as.data.frame
 df_plot <- df %>% filter(ever_legalized != 1 | state == "Colorado") %>% 
   group_by(year,ever_legalized) %>% 
@@ -9,7 +9,7 @@ plot <- ggplot(data = df_plot, aes(x = year, y = cdr, group = as.factor(ever_leg
   geom_vline(xintercept =  2012, linetype = "dashed")+
   scale_x_continuous()+
   scale_linetype_discrete(name = "", labels = c("Control States", "Colorado"))+
-  theme_minimal()
+    theme_minimal()
 
 plot
 
